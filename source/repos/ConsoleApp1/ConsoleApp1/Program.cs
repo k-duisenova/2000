c38a6d2a125ff8aed1ace2f93e0ad800901d856d
Kuralay <kuralaiduisenova97@gmail.com>
2019-02-06 21:01:25 +0600

    new

---
+++ source/repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineA = Console.ReadLine();
+            string lineB = Console.ReadLine();
+            Console.WriteLine(lineA + lineB);
+
+            int a = int.Parse(lineA);
+            int b = int.Parse(lineB);
+            Console.WriteLine(a + b);
+        }
+    }
+}
