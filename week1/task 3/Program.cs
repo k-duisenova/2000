1de7cf2643ca7d5587386fa139df7c96fcf81df3
Kuralay <kuralaiduisenova97@gmail.com>
2019-02-08 01:38:36 +0600

    changed a little

--- week1/task 3/Program.cs
+++ week1/task 3/Program.cs
@@ -7,17 +7,35 @@ using System.Threading.Tasks;
 namespace task3
 {
     class Program
-    {
+    {/// <summary>
+    /// making new array with doubled elements of the existing array
+    /// </summary>
+    /// <param name="a"></param>
+    /// <returns></returns>
+        static int[] MakeDoubleArray(int[] a)
+        {
+            int[] arr = new int[a.Length * 2];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                arr[2 * i] = arr[2 * i + 1] = a[i]; // doubling one integer number
+            }
+
+            return arr;
+        }
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine()); // n - size of the array a
             int[] a = new int[n];
             string s = Console.ReadLine();
             a = s.Split(' ').Select(int.Parse).ToArray(); // making the array n from entered numbers
-            for (int i = 0; i < a.Length; ++i)
+
+            int[] arr = new int[2 * n]; // new array with doubled size
+            arr = MakeDoubleArray(a);
+
+            for (int i = 0; i < arr.Length; ++i)
             {
-                Console.Write(a[i] + " ");
-                Console.Write(a[i] + " "); // doubling the number
+                Console.Write(arr[i] + " ");
             }
         }
     }
