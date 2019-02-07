8610081e184471b35f50daf349007fa12eae870c
Kuralay <kuralaiduisenova97@gmail.com>
2019-02-08 01:02:21 +0600

    new commit

---
+++ week2/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {/// <summary>
+    /// defining whether it is palindrome or not
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+        public static bool IsPalindrome(string s, int k)
+        {
+            for (int i = 0; i < k / 2; i++)
+            {
+                if (s[i] != s[k - i - 1])
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C: \Users\UserPC\Desktop\1.txt.txt"); // reading the numbers from the file
+            int k = text.Length; 
+            if(IsPalindrome(text,k) == true)
+            {
+                Console.WriteLine("Yes");
+     
+            } else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ week2/task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {   /// <summary>
+    /// defining prime numbers
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+        public static bool IsPrime(int x)
+        {
+            if (x <= 1) // by definition of prime numbers: they have to be more than 1
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(x); ++i)
+            {
+                if (x % i == 0) //by definition prime numbers can be divided by 1 and themselves only
+                {
+                    return false;
+                }
+            }
+            return true; // if two conditions above are not hold, it is prime number
+        }
+
+        static void Main(string[] args)
+        {
+            string numbers = System.IO.File.ReadAllText(@"C:\Users\UserPC\Desktop\1.txt.txt"); // reading the numbers from the file
+            int[] a = new int[numbers.Length/2]; // divided by 2 because the line contains spaces
+            a = numbers.Split(' ').Select(int.Parse).ToArray();
+            List<int> list = new List<int>(); // creating list to store prime numbers
+            for (int i = 0; i < a.Length; ++i)
+            {
+                if (IsPrime(a[i]) == true)
+                {
+                    list.Add(a[i]); // adding prime numbers to the list
+                    
+                }
+            }
+ 
+            var result = String.Join(" ", list.ToArray()); // converting the list into string
+            System.IO.File.WriteAllText(@"C:\Users\UserPC\Desktop\2.txt", result); // writing prime numbers to the new file
+
+        }
+    }
+}
