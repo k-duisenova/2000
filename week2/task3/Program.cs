be18807eb9744a587a4681cdc0864dc107ad2e95
Kuralay <kuralaiduisenova97@gmail.com>
2019-02-14 14:32:08 +0600

    upd

--- week2/task3/Program.cs
+++ week2/task3/Program.cs
@@ -11,21 +11,25 @@ namespace task3
     {
         static void Main(string[] args)
         {
-            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\UserPC\week1");
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\UserPC\week1"); // defining the directory(folder)
             PrintInfo(dir, 0);
         }
-
+        /// <summary>
+        /// printing the content of the given folder
+        /// </summary>
+        /// <param name="fsi"></param>
+        /// <param name="k"></param>
         static void PrintInfo(FileSystemInfo fsi, int k)
         {
-            string s = new string(' ', k);
-            Console.WriteLine(s + fsi.Name);
+            string s = new string(' ', k); 
+            Console.WriteLine(s + fsi.Name); // writing the names of the content of the directory
 
-            if (fsi.GetType() == typeof(DirectoryInfo))
+            if (fsi.GetType() == typeof(DirectoryInfo)) 
             {
-                FileSystemInfo[] arr = ((DirectoryInfo)fsi).GetFileSystemInfos();
+                FileSystemInfo[] arr = ((DirectoryInfo)fsi).GetFileSystemInfos(); // content of the folder = array of files
                 for (int i = 0; i < arr.Length; ++i)
                 {
-                    PrintInfo(arr[i], k + 3);
+                    PrintInfo(arr[i], k + 3); // k+3 in order to write down the content in structured way(creating spaces)
                 }
             }
         }
