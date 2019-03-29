b170d7495047d91fbb6b790d3d76aba684959fc2
Kuralay <47269921+k-duisenova@users.noreply.github.com>
2019-03-29 20:20:11 +0600

    update

---
+++ week7/week7lab6task1/week7lab6task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace week7lab6task1
+{
+    class Program
+    {
+        static void startThread()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(Thread.CurrentThread.Name + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[5];
+            for (int i = 1; i <= 4; i++)
+            {
+                threads[i] = new Thread(startThread);
+                threads[i].Name = i.ToString();
+                threads[i].Start();
+            }
+            
+
+        }
+       
+
+    }
+}
---
+++ week7/week7lab6task2/week7lab6task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace week7lab6task2
+{
+    public class MyThread
+    {
+        //Thread ThreadField = new Thread();
+        public MyThread(string name)
+        {
+            
+        }
+        
+        public void StartThread()
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThread t1 = new MyThread("thread1");
+            MyThread t2 = new MyThread("thread2");
+            MyThread t3 = new MyThread("thread3");
+
+            t1.StartThread();
+            t2.StartThread();
+            t3.StartThread();
+        }
+    }
+}
